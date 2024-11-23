af2ee9c171fbeb90591621bfe059e3cbea7d6e13
Amit Sarvate <amitsarvate@Amits-MacBook-Air.local>
2024-11-23 16:48:55 -0500

    partially done CNN

--- .ipynb_checkpoints/models-checkpoint.py
+++ .ipynb_checkpoints/models-checkpoint.py
@@ -1,18 +1,33 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-import torch
+
+class ConvolutionalNeuralNetwork(nn.Module):
+    def __init__(self, vocab_size, embed_dim, num_classes, kernel_sizes, num_filters):
+        super(ConvolutionalNeuralNetwork, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+        self.convs = nn.ModuleList([
+            nn.Conv2d(1, num_filters, (k, embed_dim)) for k in kernel_sizes
+        ])
+        self.fc = nn.Linear(len(kernel_sizes) * num_filters, num_classes)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        x = x.unsqueeze(1)
+        conv_outs = [F.relu(conv(x)).squeeze(3) for conv in self.convs]
+        pooled_outs = [F.max_pool1d(c_out, c_out.size(2)).squeeze(2) for c_out in conv_outs]
+        x = torch.cat(pooled_outs, dim=1)
+        x = self.fc(x)
+        return x
 
 class FeedforwardNeuralNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(FeedforwardNeuralNetwork, self).__init__()
-
         self.fc1 = nn.Linear(input_dim, hidden_dim)
         self.relu1 = nn.ReLU()
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
         self.relu2 = nn.ReLU()
         self.fc3 = nn.Linear(hidden_dim, output_dim)
-
 
     def forward(self, x):
         x = self.fc1(x)
@@ -20,6 +35,5 @@ class FeedforwardNeuralNetwork(nn.Module):
         x = self.fc2(x)
         x = self.relu2(x)
         x = self.fc3(x)
-        return x  
+        return x
 
-    
